3221d543a32807f58e7a7aa7db573293b14ae369
Ronnen Nagal <nagalr@gmail.com>
2021-01-18 14:42:10 +0200

    Add clarification comments to App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,10 +14,13 @@ public class App {
 
     public static void main(String[] args) {
 
+        // try-with-resources to close the session at the end
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
 
+            // start a transaction
             session.beginTransaction();
 
+            // creates two student Objects
             Student student = new Student();
             student.setFirstName("John");
             student.setLastName("Bloch");
@@ -28,15 +31,21 @@ public class App {
             student2.setLastName("Thompson");
             student2.setContactNo("+1-416-575-1255");
 
+            // save the students Objects
             session.save(student);
             session.save(student2);
+
+            // commit the transaction
             session.getTransaction().commit();
 
+            // Query the table for all Students
             Query<Student> q = session.createQuery("From Student", Student.class);
 
+            // prints the Student table size
             List<Student> resultList = q.list();
             System.out.println("total students: " + resultList.size());
 
+            // prints the content of 'Students'
             for (Student s : resultList) {
                 System.out.println("student : " + s);
             }
